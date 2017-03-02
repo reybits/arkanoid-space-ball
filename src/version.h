0d0f28a7b68e596d8e5be6f535393c4fdbeffa14
Andrey A. Ugolnik <andrey@ugolnik.info>
2017-03-02 21:58:16 +0300

    File version.h restored.

---
+++ src/version.h
@@ -0,0 +1,14 @@
+/**********************************************\
+*
+*  Copyright (C) 2006 by Andrey A. Ugolnik
+*  http://www.ugolnik.info
+*  andrey@ugolnik.info
+*
+\**********************************************/
+
+#pragma once
+
+extern const char* APP_Title;
+extern const int APP_VerMajor;
+extern const int APP_VerMinor;
+extern const int APP_VerRelease;
